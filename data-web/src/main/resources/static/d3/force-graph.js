dc89b387127acb2a58d26242315e446c8b7a7ca4
x-lin <x.lin@gmx.net>
2016-01-13 02:44:45 +0100

    improvements for d3 force graph: added double click event, some performance improvements, made node text unselectable to avoid text marking

--- data-web/src/main/resources/static/d3/force-graph.js
+++ data-web/src/main/resources/static/d3/force-graph.js
@@ -4,13 +4,15 @@ d3Service.factory('ForceGraph',
         D3Utility
     ) {
 
-        var currentGraph;
-        var currentSvg;
+        var currentGraph = null;
+        var currentSvg = null;
 
         return {
             updateSize: function(width, height) {
-                console.log("updating");
-                currentSvg.attr("width", width).attr("height", height);
+                if(currentGraph === null) {
+                    return;
+                }
+
                 currentGraph.size([width, height]).resume();
 
                 //TODO improve
@@ -18,9 +20,7 @@ d3Service.factory('ForceGraph',
                     .each(function(d){d.fixed = false;})
                     .classed("fixed", false);
             },
-            renderForceGraph: function(graph, width, height, scope) {
-                console.log("rendering force graph");
-
+            renderForceGraph: function(data, width, height, scope) {
                 //var svg = D3Utility.getResponsiveSvg("#d3box", width, height).
                 //    attr("class", "force-graph");
                 d3.select("#d3box").select("svg").remove();
@@ -34,62 +34,75 @@ d3Service.factory('ForceGraph',
                 var color = d3.scale.category20();
 
                 var force = d3.layout.force()
-                    .charge(-700)
+                    .charge(-750)
                     .linkDistance(50)
+                    .gravity(0.2)
                     .size([width, height])
-                    .nodes(graph.nodes)
-                    .links(graph.edges)
-                    .start();
-
-                var link = svg.selectAll(".link")
-                    .data(graph.edges)
-                    .enter().append("line")
-                    .attr("class", "link")
-                    //.style("stroke-width", function(d) { return Math.sqrt(d.value); })
-                    ;
+                    .nodes(data.nodes)
+                    .links(data.edges)
+                    .on('start', animation);
 
                 //define drag events
                 var drag = force.drag()
                     .on("dragstart", dragstart)
                     .on("drag", dragmove);
 
-                var node = svg.selectAll(".node")
-                    .data(graph.nodes)
-                    .enter().append("g")
+                var link = svg.selectAll(".link");
+                var node = svg.selectAll(".node");
+
+                //force.on("tick", function() {
+                //    link.attr("x1", function(d) { return d.source.x; })
+                //        .attr("y1", function(d) { return d.source.y; })
+                //        .attr("x2", function(d) { return d.target.x; })
+                //        .attr("y2", function(d) { return d.target.y; });
+                //
+                //    node.attr("transform", function(d) {return "translate(" + d.x + "," + d.y + ")"});
+                //});
+
+
+                var restart = function(){
+                    console.log("restarting");
+                    console.log(data);
+
+                    link = link.data(data.edges);
+                    link.enter().append("line")
+                    .attr("class", "link")
+                    //.style("stroke-width", function(d) { return Math.sqrt(d.value); })
+                    ;
+
+                    node = node.data(data.nodes);
+                    node.enter().append("g")
                     .attr("class", "g")
                     .style("fill", function(d) { return color(d.group); })
-                    .attr("uib-popover", function(d) {
-                        return d.key;
-                    })
-                    .attr("popover-trigger", "outsideClick")
-                    .attr("popover-append-to-body", "true")
+                    //.attr("uib-popover", function(d) {
+                    //    return d.key;
+                    //})
+                    //.attr("popover-trigger", "outsideClick")
+                    //.attr("popover-append-to-body", "true")
+                    //.attr("uib-popover-template", "dynamicPopover.templateUrl")
+                    //.attr("popover-title", "{{dynamicPopover.title}}")
                     .on("click", mouseclick)
                     .on("contextmenu", rightclick)
                     .on("dblclick", doubleclick)
                     .call(drag)
                     ;
 
-                node.append("circle")
-                    .attr("class", "circle")
-                    .attr("r", 20);
+                    node.append("circle")
+                        .attr("class", "circle")
+                        .attr("r", 20);
 
+                    node.append("text")
+                        .attr("class", "force-text unselectable")
+                        .text(function(d) { return d.key; })
+                        .call(function(){
+                            $compile($("#d3box"))(scope);
+                        });
 
-                node.append("text")
-                    .attr("class", "force-text")
-                    .text(function(d) { return d.key; })
-                    .call(function(){
-                        console.log("doing something")
-                        $compile($("#d3box"))(scope);
-                    });
+                    console.log("starting force")
+                    force.start();
+                }
 
-                force.on("tick", function() {
-                    link.attr("x1", function(d) { return d.source.x; })
-                        .attr("y1", function(d) { return d.source.y; })
-                        .attr("x2", function(d) { return d.target.x; })
-                        .attr("y2", function(d) { return d.target.y; });
-
-                    node.attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"});
-                });
+                restart();
 
                 //fix position of node after dragged by user
                 function dragstart(d) {
@@ -106,17 +119,59 @@ d3Service.factory('ForceGraph',
                 }
 
                 function mouseclick(d) {
-                    console.log("clicked on me");
+
                 }
 
                 function rightclick(d) {
-                    //return false;
                     //d3.event.preventDefault();
                 }
 
                 function doubleclick(d) {
-                    console.log("double clicked");
-                    //d3.event.preventDefault();
+                    var nr = 5;
+
+                    for(var i = 0; i<nr; i++) {
+                        var n = {};
+                        n.key = "testing node";
+                        n.group = 3;
+                        var point = d3.mouse(this);
+                        n.x = point[0];
+                        n.y = point[1];
+
+                        data.nodes.push(n);
+
+                        var e = {
+                            source: d.index,
+                            target: data.nodes.length - 1
+                        };
+                        data.edges.push(e);
+                    }
+
+                    restart();
+
+                    force.start();
+                }
+
+                //speed up animation by 10
+                function animation() {
+                    var ticksPerRender = 10;
+                    requestAnimationFrame(function render() {
+                        console.log("rendering");
+                        for (var i = 0; i < ticksPerRender; i++) {
+                            force.tick();
+                        }
+
+                        link
+                            .attr('x1', function(d) { return d.source.x; })
+                            .attr('y1', function(d) { return d.source.y; })
+                            .attr('x2', function(d) { return d.target.x; })
+                            .attr('y2', function(d) { return d.target.y; });
+
+                        node.attr("transform", function(d) {return "translate(" + d.x + "," + d.y + ")"});
+
+                        if (force.alpha() > 0) {
+                            requestAnimationFrame(render);
+                        }
+                    })
                 }
 
                 currentGraph = force;
