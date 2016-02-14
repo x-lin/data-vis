642698ec687718dc9fad80df812a3fa6f5d6d3f1
x-lin <x.lin@gmx.net>
2016-02-14 01:24:07 +0100

    added/improved commenting

--- data-web/src/main/java/at/ac/tuwien/dst/mms/PathMatchingConfig.java
+++ data-web/src/main/java/at/ac/tuwien/dst/mms/PathMatchingConfig.java
@@ -5,7 +5,7 @@ import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 /**
- * Used to turn off quirk of Spring cutting off dots from last parameter value
+ * Used to turn off quirk of Spring REST controllers cutting off dots from last parameter value
  */
 @Configuration
 public class PathMatchingConfig extends WebMvcConfigurerAdapter {
