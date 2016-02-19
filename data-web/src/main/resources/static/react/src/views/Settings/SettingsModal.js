93affc3e03dd7fd6f667b68e72e73d62dec5a8d8
x-lin <x.lin@gmx.net>
2016-02-19 23:00:22 +0100

    cleaned up and debugged settings modal and structured it nicer

--- data-web/src/main/resources/static/react/src/views/Settings/SettingsModal.js
+++ data-web/src/main/resources/static/react/src/views/Settings/SettingsModal.js
@@ -1,66 +1,8 @@
 import React from "react";
 import { connect } from "react-redux";
 
-import { toggleSetting } from "../../actions/UserActions/SettingsActions";
-
-class SettingsModal extends React.Component {
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            checkBoxes: this.initCheckboxes
-        }
-    };
-
-    initCheckboxes() {
-        const checkBoxes = {};
-
-        for(let i = 0; i < this.props.settings.length; i++) {
-            checkBoxes[this.props.settings.name] = this.props.settings.value;
-        }
-
-        return checkBoxes;
-    }
-
-    handleToggle(event) {
-        const obj = {};
-        obj[event.target.value] = !this.state.checkBoxes[event.target.value];
-        this.setState(obj);
-        this.props.toggle(event.target.value);
-    }
-
-    render() {
-        var checkboxes = this.props.settings.map((setting, index) => {
-            return <label key={index}><input type="checkbox" value={setting.name} checked={this.state[name]} onChange={(event) => this.handleToggle(event)}/>{setting.description}</label>;
-        });
-
-        return (
-            <div className="modal  fade" id={this.props.modalId} role="dialog">
-                <div className="modal-dialog">
-                    <div className="modal-content ">
-                        <div className="modal-header ">
-                            <button type="button" className="close" data-dismiss="modal">&times;</button>
-                            <h4 className="modal-title">Settings</h4>
-                        </div>
-                        <div className="modal-body">
-                            <div className="checkbox">
-                                {checkboxes}
-                            </div>
-
-                            {/*<div className="checkbox disabled">
-                                <label><input type="checkbox" value="" disabled>Option 3</label>
-                            </div>*/}
-                        </div>
-                        <div className="modal-footer">
-                            <button type="button" className="btn btn-default" data-dismiss="modal">Close</button>
-                        </div>
-                    </div>
-
-                </div>
-            </div>
-        );
-    };
-}
+import { toggleSetting } from "../../actions/UserActions/SettingsActions"
+import SettingsModalBehView from "./SettingsModalBehView";
 
 const mapStateToProps = (state) => {
     return {
@@ -76,9 +18,7 @@ const mapDispatchProps = (dispatch) => {
     };
 };
 
-const SettingsModalConnect = connect(
+export default connect(
     mapStateToProps,
     mapDispatchProps
-)(SettingsModal);
-
-export default SettingsModalConnect;
+)(SettingsModalBehView);
