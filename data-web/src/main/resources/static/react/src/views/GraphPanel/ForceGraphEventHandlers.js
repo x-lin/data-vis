b30cb3a20f70eede74107445e7241b3a8339c92e
x-lin <x.lin@gmx.net>
2016-02-22 21:50:49 +0100

    moved event handlers for force graph to its own file, fixed bug where  new filtered edges were not being filtered out

---
+++ data-web/src/main/resources/static/react/src/views/GraphPanel/ForceGraphEventHandlers.js
@@ -0,0 +1,32 @@
+import ContextMenuBuilder from "./ContextMenu/ContextMenuBuilder";
+
+const EventHandlers = {};
+
+EventHandlers.onClickSvg = () => {
+    ContextMenuBuilder.removeAll();
+};
+
+EventHandlers.onZoomSvg = (panelElement) => {
+    ContextMenuBuilder.removeAll();
+    panelElement.attr("transform", "translate(" + d3.event.translate + ") scale(" + d3.event.scale + ")");
+};
+
+EventHandlers.onContextMenuNode = (d) => {
+    d3.event.preventDefault();
+
+    const popoverEl = ContextMenuBuilder.buildElement(d.key, d.category);
+    ContextMenuBuilder.create(popoverEl);
+    ContextMenuBuilder.show(popoverEl);
+};
+
+EventHandlers.onDoubleClickNode = (d) => {
+
+};
+
+EventHandlers.onDragStartNode = (d) => {
+    d3.event.sourceEvent.stopPropagation();
+    ContextMenuBuilder.removeAll();
+    d.fixed = true;
+};
+
+export default EventHandlers;
