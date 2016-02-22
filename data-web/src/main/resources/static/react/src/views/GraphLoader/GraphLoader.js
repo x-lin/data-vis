37dde1e3b7712f0090915dd2fe179fb056be972d
x-lin <x.lin@gmx.net>
2016-02-22 16:37:13 +0100

    code cleanup

--- data-web/src/main/resources/static/react/src/views/GraphLoader/GraphLoader.js
+++ data-web/src/main/resources/static/react/src/views/GraphLoader/GraphLoader.js
@@ -3,14 +3,6 @@ import React from "react";
 import GraphLoaderPresentation from "./GraphLoaderPresentation";
 
 export default class extends React.Component {
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            startSave: false
-        }
-    }
-
     handleSave(event) {
         const blob = new Blob([JSON.stringify(this.props.graph)], {type: "text/plain;charset=utf-8"});
         const filename = `graph-${Date.now()}.json`;
