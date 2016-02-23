8268e31d98fb2e38b93000bd921e2a8e27c4641a
x-lin <x.lin@gmx.net>
2016-02-23 03:07:32 +0100

    added more settings

--- data-web/src/main/resources/static/react/src/config/Settings.js
+++ data-web/src/main/resources/static/react/src/config/Settings.js
@@ -1,15 +1,31 @@
 export const ADD_TO_GRAPH_ON_SEARCH = "RENDER_NEW_GRAPH_ON_SEARCH";
 export const DISABLED_OPACITY_VALUE = "DISABLED_OPACITY_VALUE";
+export const DISABLE_SELECTION_OF_FILTERED_NODES = "DISABLE_SELECTION_OF_FILTERED_NODES";
+export const SET_NODE_POSITIONS_FIXED = "SET_NODE_POSITIONS_FIXED";
+export const SHOW_CONTEXT_MENU = "SHOW_CONTEXT_MENU";
 
 export default [
     {
         name: ADD_TO_GRAPH_ON_SEARCH,
-        description: "Add to current graph on new search",
+        description: "Add to current graph on new search.",
         value: false
-    },
-    //{
-    //    name: DISABLED_OPACITY_VALUE,
-    //    description: "Set opacity of disabled nodes/edges",
-    //    value: 0.2
-    //}
+    }, {
+        name: DISABLED_OPACITY_VALUE,
+        description: "Set opacity of filtered nodes/edges.",
+        value: 0.3,
+        min: 0,
+        max: 1
+    }, {
+        name: DISABLE_SELECTION_OF_FILTERED_NODES,
+        description: "Disable expansion and context menus of filtered nodes.",
+        value: false
+    }, {
+        name: SET_NODE_POSITIONS_FIXED,
+        description: "Disable floating nodes.",
+        value: false
+    }, {
+        name: SHOW_CONTEXT_MENU,
+        description: "Show context menu on right click.",
+        value: true
+    }
 ];
