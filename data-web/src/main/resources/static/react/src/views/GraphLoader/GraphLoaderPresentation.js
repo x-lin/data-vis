b9c75b091bfef00e1325bb71e905d02b42d64be9
x-lin <x.lin@gmx.net>
2016-02-23 03:07:20 +0100

    changed button style slightly

--- data-web/src/main/resources/static/react/src/views/GraphLoader/GraphLoaderPresentation.js
+++ data-web/src/main/resources/static/react/src/views/GraphLoader/GraphLoaderPresentation.js
@@ -5,15 +5,21 @@ export default ( {
     onLoad
     } ) => {
     return (
-        <div className="row">
-            <button type="button" className="btn btn-default" onClick={onSave}>
-                <span className="glyphicon glyphicon-floppy-disk"/>
-                Save Graph as File
-            </button>
-            <div className="btn btn-default btn-file">
-                <span className="glyphicon glyphicon-open"/>
-                <input type="file" onChange={(event) => onLoad(event)} />
-                Load Graph File
+        <div>
+            <div className="btn-group" role="group" aria-label="Browse at source site">
+                <div className="btn-group" role="group">
+                    <div className="btn btn-default" onClick={onSave}>
+                        <span className="glyphicon glyphicon-floppy-disk"/>
+                        Save Graph as File
+                    </div>
+                </div>
+                <div className="btn-group" role="group">
+                    <div className="btn btn-default btn-file">
+                        <span className="glyphicon glyphicon-open"/>
+                        <input type="file" onChange={(event) => onLoad(event)} />
+                        Load Graph File
+                    </div>
+                </div>
             </div>
         </div>
     );
