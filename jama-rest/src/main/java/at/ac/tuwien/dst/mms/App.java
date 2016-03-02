d2ae39cabf3f233e95124cbc44a0fbac0e65534b
x-lin <x.lin@gmx.net>
2016-03-02 11:07:26 +0100

    added empty jama rest module

---
+++ jama-rest/src/main/java/at/ac/tuwien/dst/mms/App.java
@@ -0,0 +1,13 @@
+package at.ac.tuwien.dst.mms;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
